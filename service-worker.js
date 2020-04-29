20d876a7472a0c8d6eb97e4caeda80bb9bcfff89
LavanyaPolamarasetty <51402194+LavanyaPolamarasetty@users.noreply.github.com>
2020-04-29 11:05:56 +0530

    my file

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/swamiprofile/precache-manifest.f19f25722ed3d576e396427c0dccd138.js"
+  "/react_profile/precache-manifest.f19f25722ed3d576e396427c0dccd138.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/swamiprofile/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_profile/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
